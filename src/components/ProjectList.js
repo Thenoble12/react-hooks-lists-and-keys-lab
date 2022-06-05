1b0c1ae930495eb17e12d6ea58d60a4b6492cbdd
Elijah Noble <elijahnobledev@gmail.com>
2022-06-04 19:17:34 -0700

    first commit

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,18 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map(projectItem => {
+          return (
+            <ProjectItem 
+              key={projectItem.id} 
+              name={projectItem.name} 
+              about={projectItem.about}
+              technologies={projectItem.technologies}
+            />
+          )             
+        })}
+      </div>
     </div>
   );
 }
